05c50de970d3989f8ef8eac699345eb270fa0568
Matt Rothstein <it@4beauty.com>
2018-03-28 23:42:55 -0400

    Customize Template

--- template.rb
+++ template.rb
@@ -13,7 +13,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
-      "https://github.com/excid3/jumpstart.git",
+      "https://github.com/mattrothstein/jumpstart.git",
       tempdir
     ].map(&:shellescape).join(" ")
 
@@ -26,23 +26,16 @@ def add_template_repository_to_source_path
 end
 
 def add_gems
-  gem 'administrate', '~> 0.8.1'
+  gem 'haml'
+  gem 'haml-rails'
   gem 'data-confirm-modal', '~> 1.6.2'
   gem 'devise', '~> 4.4.3'
   gem 'devise-bootstrapped', github: 'excid3/devise-bootstrapped', branch: 'bootstrap4'
-  gem 'devise_masquerade', '~> 0.6.0'
   gem 'font-awesome-sass', '~> 4.7'
   gem 'gravatar_image_tag', github: 'mdeering/gravatar_image_tag'
   gem 'jquery-rails', '~> 4.3.1'
   gem 'bootstrap', '~> 4.0.0.beta'
   gem 'mini_magick', '~> 4.8'
-  gem 'webpacker', '~> 3.0'
-  gem 'sidekiq', '~> 5.0'
-  gem 'foreman', '~> 0.84.0'
-  gem 'omniauth-facebook', '~> 4.0'
-  gem 'omniauth-twitter', '~> 1.4'
-  gem 'omniauth-github', '~> 1.3'
-  gem 'whenever', require: false
 end
 
 def set_application_name
@@ -85,9 +78,6 @@ def add_users
       /  # config.secret_key = .+/,
       "  config.secret_key = Rails.application.credentials.secret_key_base"
   end
-
-  # Add Devise masqueradable to users
-  inject_into_file("app/models/user.rb", "omniauthable, :masqueradable, :", after: "devise :")
 end
 
 def add_bootstrap
@@ -102,84 +92,14 @@ def add_bootstrap
   )
 end
 
+def convert_to_haml
+  run "rake haml:erb2haml"
+end
+
 def copy_templates
   directory "app", force: true
   directory "config", force: true
   directory "lib", force: true
-
-  route "get '/terms', to: 'home#terms'"
-  route "get '/privacy', to: 'home#privacy'"
-end
-
-def add_webpack
-  rails_command 'webpacker:install'
-end
-
-def add_sidekiq
-  environment "config.active_job.queue_adapter = :sidekiq"
-
-  insert_into_file "config/routes.rb",
-    "require 'sidekiq/web'\n\n",
-    before: "Rails.application.routes.draw do"
-
-  insert_into_file "config/routes.rb",
-    "  authenticate :user, lambda { |u| u.admin? } do\n    mount Sidekiq::Web => '/sidekiq'\n  end\n\n",
-    after: "Rails.application.routes.draw do\n"
-end
-
-def add_foreman
-  copy_file "Procfile"
-end
-
-def add_announcements
-  generate "model Announcement published_at:datetime announcement_type name description:text"
-  route "resources :announcements, only: [:index]"
-end
-
-def add_notifications
-  generate "model Notification recipient_id:integer actor_id:integer read_at:datetime action:string notifiable_id:integer notifiable_type:string"
-  route "resources :notifications, only: [:index]"
-end
-
-def add_administrate
-  generate "administrate:install"
-
-  gsub_file "app/dashboards/announcement_dashboard.rb",
-    /announcement_type: Field::String/,
-    "announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
-
-  gsub_file "app/controllers/admin/application_controller.rb",
-    /# TODO Add authentication logic here\./,
-    "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
-end
-
-def add_multiple_authentication
-    insert_into_file "config/routes.rb",
-    ', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }',
-    after: "  devise_for :users"
-
-    generate "model Service user:references provider uid access_token access_token_secret refresh_token expires_at:datetime auth:text"
-
-    template = """
-  if Rails.application.secrets.facebook_app_id.present? && Rails.application.secrets.facebook_app_secret.present?
-    config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'email,user_posts'
-  end
-
-  if Rails.application.secrets.twitter_app_id.present? && Rails.application.secrets.twitter_app_secret.present?
-    config.omniauth :twitter, Rails.application.secrets.twitter_app_id, Rails.application.secrets.twitter_app_secret
-  end
-
-  if Rails.application.secrets.github_app_id.present? && Rails.application.secrets.github_app_secret.present?
-    config.omniauth :github, Rails.application.secrets.github_app_id, Rails.application.secrets.github_app_secret
-  end
-    """.strip
-
-    insert_into_file "config/initializers/devise.rb", "  " + template + "\n\n",
-          before: "  # ==> Warden configuration"
-end
-
-def add_whenever
-  run "wheneverize ."
 end
 
 # Main setup
@@ -189,26 +109,14 @@ add_gems
 
 after_bundle do
   set_application_name
-  add_users
   add_bootstrap
-  add_sidekiq
-  add_foreman
-  add_webpack
-  add_announcements
-  add_notifications
-  add_multiple_authentication
+  convert_to_haml
 
   copy_templates
 
   # Migrate
   rails_command "db:create"
   rails_command "db:migrate"
-
-  # Migrations must be done before this
-  add_administrate
-
-  add_whenever
-
 
   git :init
   git add: "."
